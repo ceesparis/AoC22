e67ca37e07f23a8cce59dc13bf6954620cd0ba6c
Cees Paris <cees.paris@acorel.nl>
2022-12-02 16:17:25 +0100

    refactor day2: Koen's idea for circular list

--- src/main/groovy/days/day2/Day2.groovy
+++ src/main/groovy/days/day2/Day2.groovy
@@ -2,52 +2,35 @@
 String filePath = "/Users/cees/dev/repos/AoC23/src/main/resources/inputs/day2.txt"
 File file = new File(filePath);
 
-rock = "rock"
-paper = "paper"
-scissors = "scissors"
 
-def normMap = [A : rock, B : paper, C : scissors ]
-def codeMap = [X : rock, Y: paper, Z: scissors]
-def scoreMap = [rock: 1, paper: 2, scissors: 3]
-def winsFrom = [rock : paper, paper : scissors, scissors : rock]
-def losesFrom = [rock : scissors, paper : rock, scissors : paper]
-
-
-determineScorePartOne = { String left, String right ->
-    String leftHand = normMap[left]
-    String rightHand = codeMap[right]
-    if (leftHand == rightHand) return 3 + scoreMap[rightHand]
-    if((leftHand == rock && rightHand == paper) || (leftHand == paper && rightHand == scissors) || (leftHand == scissors && rightHand == rock)) {
-        return 6 + scoreMap[rightHand]
-    }
-    return scoreMap[rightHand]
+List<String> circList = ["A", "B", "C"]
+List<String> codeCircList = ["X", "Y", "Z"]
+determineScorePartOneCircList = { String left, String right ->
+    Integer leftIndex = circList.findIndexOf {it == left}
+    Integer rightIndex = codeCircList.findIndexOf {it == right}
+    if ((leftIndex + 1) % 3 ==  rightIndex) return 6 + rightIndex + 1
+    if ((rightIndex + 1) % 3 == leftIndex) return rightIndex  + 1
+    return 3 + rightIndex + 1
 }
 
 score = 0
-
-file.eachLine(line -> {
-    List lineList =  line.split(" ")
-    score += determineScorePartOne(lineList[0], lineList[1])
-})
-
-println score
-score = 0
-
-determineScorePartTwo = { String left, String right ->
-    String leftHand = normMap[left]
-    if (right == "X") return scoreMap[losesFrom[leftHand]]
-    if (right == "Y") return scoreMap[leftHand] + 3
-    if (right == "Z") return scoreMap[winsFrom[leftHand]] + 6
-}
-
 file.eachLine( line -> {
     List lineList =  line.split(" ")
-    score += determineScorePartTwo(lineList[0], lineList[1])
+    score += determineScorePartOneCircList(lineList[0], lineList[1])
 })
 
 println score
 
+determineScorePartTwoCircList = { String left, String right ->
+    if (right == "X") return ((circList.findIndexOf {it == left} + 2) % 3) + 1
+    if (right == "Y") return circList.findIndexOf {it == left} + 1 + 3
+    if (right == "Z") return ((circList.findIndexOf {it == left} + 1) % 3) + 1 + 6
+}
 
+score = 0
+file.eachLine( line -> {
+    List lineList =  line.split(" ")
+    score += determineScorePartTwoCircList(lineList[0], lineList[1])
+})
 
-
-
+println score
